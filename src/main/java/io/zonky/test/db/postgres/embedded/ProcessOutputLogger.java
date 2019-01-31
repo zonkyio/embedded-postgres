736c4272298953fadf7b63f6351e12fd5d868257
Steven Schlansker <stevenschlansker@gmail.com>
2019-01-31 17:24:46 +0100

    Clean up logger names take 2 (#104)
    * ProcessOutputLogger: clean up logger name and use just the pid rather than `java.lang.UNIXProcess@42f233`
    
    take 2 includes java 8/9+ support and fallback
    
    * ProcessOutputLogger: skip `null` lines

--- src/main/java/io/zonky/test/db/postgres/embedded/ProcessOutputLogger.java
+++ src/main/java/io/zonky/test/db/postgres/embedded/ProcessOutputLogger.java
@@ -18,7 +18,11 @@ import org.slf4j.Logger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
@@ -48,7 +52,7 @@ final class ProcessOutputLogger implements Runnable {
         try {
             while (process.isAlive()) {
                 try {
-                    logger.info(reader.readLine());
+                    Optional.ofNullable(reader.readLine()).ifPresent(logger::info);
                 } catch (final IOException e) {
                     logger.error("while reading output", e);
                     return;
@@ -64,9 +68,23 @@ final class ProcessOutputLogger implements Runnable {
     }
 
     static void logOutput(final Logger logger, final Process process, final String processName) {
-        final String threadName = isNotBlank(processName) ? processName + "@" + process.hashCode() : process.toString();
+        final String threadName = (isNotBlank(processName) ? processName : "unknown") + ":" + processId(process);
         final Thread t = new Thread(new ProcessOutputLogger(logger, process));
         t.setName(threadName);
         t.start();
+    }
+
+    private static String processId(Process process) {
+        try { // java 9+
+            return String.format("pid(%s)", MethodHandles.lookup().findVirtual(Process.class, "pid", MethodType.methodType(long.class)).invoke(process));
+        } catch (Throwable ignored) {} // NOPMD since MethodHandles.invoke throws Throwable
+
+        try { // openjdk / oraclejdk 8
+            final Field pid = process.getClass().getDeclaredField("pid");
+            pid.setAccessible(true);
+            return String.format("pid(%s)", pid.getInt(process));
+        } catch (Exception ignored) {} // NOPMD
+
+        return String.format("id(%s)", process.hashCode());
     }
 }
